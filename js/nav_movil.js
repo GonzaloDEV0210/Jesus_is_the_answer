addef48d1568eeb0add65edb2f52d3a0de760b7d
Gonzalo Palma <133717685+GonzaloDEV0210@users.noreply.github.com>
2025-07-12 22:45:53 -0500

    animacion de la opcion lenguaje en movil

--- js/nav_movil.js
+++ js/nav_movil.js
@@ -4,3 +4,16 @@ const nav_movil = document.querySelector('.nav_movil');
 cerrar_nav_movil.addEventListener('click', ()=>{
     nav_movil.classList.add('cerrar_nav_movil');
 });
+
+const selected_lang_movil = document.querySelector('.selected_lang_movil');
+const lenguage_combo_movil = document.querySelector('.lenguage_combo_movil');
+
+selected_lang_movil.addEventListener('click', ()=>{
+    if (lenguage_combo_movil.classList.contains('cerrar_lenguage_movil')) {
+        lenguage_combo_movil.classList.add('open_lenguage_movil');
+        lenguage_combo_movil.classList.remove('cerrar_lenguage_movil');
+    } else {
+        lenguage_combo_movil.classList.add('cerrar_lenguage_movil');
+        lenguage_combo_movil.classList.remove('open_lenguage_movil');
+    }
+})
