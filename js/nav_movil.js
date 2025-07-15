0b1d0e6fb80024b1ee2c6ad9764b61ef43950589
Gonzalo Palma <133717685+GonzaloDEV0210@users.noreply.github.com>
2025-07-14 20:21:57 -0500

    nav movil finished

--- js/nav_movil.js
+++ js/nav_movil.js
@@ -3,6 +3,7 @@ const nav_movil = document.querySelector('.nav_movil');
 
 cerrar_nav_movil.addEventListener('click', ()=>{
     nav_movil.classList.add('cerrar_nav_movil');
+    nav_movil.classList.remove('abrir_nav_movil');
 });
 
 const selected_lang_movil = document.querySelector('.selected_lang_movil');
@@ -16,4 +17,12 @@ selected_lang_movil.addEventListener('click', ()=>{
         lenguage_combo_movil.classList.add('cerrar_lenguage_movil');
         lenguage_combo_movil.classList.remove('open_lenguage_movil');
     }
+})
+
+/* abrir nav */
+const container_barra = document.querySelector('.container_barra');
+
+container_barra.addEventListener('click', ()=>{
+    nav_movil.classList.remove('cerrar_nav_movil');
+    nav_movil.classList.add('abrir_nav_movil');
 })
