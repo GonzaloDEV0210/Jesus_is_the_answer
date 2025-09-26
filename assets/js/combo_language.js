e4e25e79291f31e07ecb4f2bf379494d3c51ffc5
Gonzalo Palma <133717685+GonzaloDEV0210@users.noreply.github.com>
2025-09-25 21:18:05 -0500

    me quedé en 1024px

--- assets/js/combo_language.js
+++ assets/js/combo_language.js
@@ -4,4 +4,14 @@ const combo = document.querySelector('.combo_select_language');
 
 combo.addEventListener('click', ()=> {
     elemento.classList.toggle('abrir_combo_language');
+    console.log(10)
+});
+
+/* DESKTOP */
+
+const option_nav_language_d = document.querySelector('.option_nav_language_d');
+const cont_options_combo_language_d = document.querySelector('.cont_options_combo_language_d');
+
+option_nav_language_d.addEventListener('click', ()=>{
+    cont_options_combo_language_d.classList.toggle('abrir_combo_language');
 });
