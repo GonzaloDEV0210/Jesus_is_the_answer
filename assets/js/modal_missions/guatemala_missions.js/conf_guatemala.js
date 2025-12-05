084b536ee59355eb19dfaf04bf80666021acc52f
Gonzalo_Palma <gonzalopalmadev0210@gmail.com>
2025-12-04 19:52:13 -0500

    avanzando con las animaciones

--- assets/js/modal_missions/guatemala_missions.js/conf_guatemala.js
+++ assets/js/modal_missions/guatemala_missions.js/conf_guatemala.js
@@ -1,6 +1,6 @@
 const icon_cerrar_modal_images = document.querySelector('.icon_cerrar_modal_images');
 const modal = document.querySelector('.back_modal_Images_guatemala');
-const abrir_modal = document.querySelector('.nombre_link_gallery_completa');
+const abrir_modal = document.querySelector('.link_modal_gallery_completa');
 
 abrir_modal.addEventListener('click', ()=>{
     modal.classList.remove('cerrar_modal_images');
